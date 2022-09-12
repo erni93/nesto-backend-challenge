4e780b74c5cebcc0ee7ffaaba2be01d56fc3fc7f
Ernesto del valle <erni.delvalle@outlook.com>
2022-09-11 22:18:31 -0400

    add gorilla router

--- service/main.go
+++ service/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"backend-api/router"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello")
+	authorsRouter := router.AuthorsRouter{}
+	booksRouter := router.BooksRouter{}
+	erasRouter := router.ErasRouter{}
+	genresRouter := router.GenresRouter{}
+	sizesRouter := router.SizesRouter{}
+
+	port := ":5001"
+	router := mux.NewRouter()
+	router.Use(corsMiddleware)
+	router.HandleFunc("/api/v1/authors", authorsRouter.GetAuthorsHandler).Methods("GET")
+	router.HandleFunc("/api/v1/books", booksRouter.GetBooksHandler).Methods("GET")
+	router.HandleFunc("/api/v1/eras", erasRouter.GetErasHandler).Methods("GET")
+	router.HandleFunc("/api/v1/genres", genresRouter.GetGenresHandler).Methods("GET")
+	router.HandleFunc("/api/v1/sizes", sizesRouter.GetSizesHandler).Methods("GET")
+
+	http.Handle("/", router)
+	log.Printf("Application listening on port %s", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
