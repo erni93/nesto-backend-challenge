2495eb6465cf44d1e153a9be800d3c774cb8ab4c
Ernesto del valle <erni.delvalle@outlook.com>
2022-09-12 23:05:58 -0400

    read books filters

--- service/repository/book-repository.go
+++ service/repository/book-repository.go
@@ -8,11 +8,21 @@ import (
 	sq "github.com/Masterminds/squirrel"
 )
 
+type GetBooksFilters struct {
+	Authors  []int64
+	Genres   []int64
+	MinPages *int32
+	MaxPages *int32
+	MinYear  *int32
+	MaxYear  *int32
+	Limit    *int32
+}
+
 type BookRepository struct {
 	DB *sql.DB
 }
 
-func (r *BookRepository) GetBooks() ([]model.Book, error) {
+func (r *BookRepository) GetBooks(filters GetBooksFilters) ([]model.Book, error) {
 	query := sq.Select("*").From("book")
 	query = query.LeftJoin("genre on (book.genre_id = genre.id)")
 	query = query.LeftJoin("author on (book.author_id = author.id)")
@@ -21,13 +31,17 @@ func (r *BookRepository) GetBooks() ([]model.Book, error) {
 		return nil, err
 	}
 
+	return readRows(rows)
+}
+
+func readRows(rows *sql.Rows) ([]model.Book, error) {
 	books := make([]model.Book, 0)
 	for rows.Next() {
 		var book model.Book
 		var genre model.Genre
 		var author model.Author
 		var ignoreColumn database.IgnoreColumn
-		err = rows.Scan(
+		err := rows.Scan(
 			&book.Id, &book.Title, &book.YearPublished, &book.Rating, &book.Pages, ignoreColumn, ignoreColumn,
 			&genre.Id, &genre.Title,
 			&author.Id, &author.FirstName, &author.LastName,
--- service/router/books-router.go
+++ service/router/books-router.go
@@ -5,6 +5,12 @@ import (
 	response "backend-api/router/response"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrInvalidFilter = "Some filters are not valid"
 )
 
 type BooksRouter struct {
@@ -12,11 +18,104 @@ type BooksRouter struct {
 }
 
 func (router *BooksRouter) GetBooksHandler(w http.ResponseWriter, r *http.Request) {
-	books, err := router.Repository.GetBooks()
+	filters, err := getFilters(r)
+	if err != nil {
+		log.Printf("Error reading GetBooks filters: %s", err)
+		response.WriteError(w, ErrInvalidFilter)
+	}
+
+	books, err := router.Repository.GetBooks(*filters)
 	if err != nil {
 		log.Printf("Error retrieving books: %s", err)
 		response.WriteGeneralError(w)
 	} else {
 		response.WriteJsonObject(w, books)
 	}
+}
+
+func getFilters(r *http.Request) (*repository.GetBooksFilters, error) {
+	filters := &repository.GetBooksFilters{}
+
+	authorsParam := r.FormValue("authors")
+	if len(authorsParam) > 0 {
+		ids, err := splitIdList(authorsParam)
+		if err != nil {
+			return nil, err
+		}
+		filters.Authors = ids
+	}
+
+	genresParam := r.FormValue("genres")
+	if len(genresParam) > 0 {
+		ids, err := splitIdList(genresParam)
+		if err != nil {
+			return nil, err
+		}
+		filters.Genres = ids
+	}
+
+	minPagesParam := r.FormValue("min-pages")
+	if len(minPagesParam) > 0 {
+		minPages, err := strconv.ParseInt(minPagesParam, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		minPagesInt32 := int32(minPages)
+		filters.MinPages = &minPagesInt32
+	}
+
+	maxPagesParam := r.FormValue("max-pages")
+	if len(maxPagesParam) > 0 {
+		maxPages, err := strconv.ParseInt(maxPagesParam, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		maxPagesInt32 := int32(maxPages)
+		filters.MaxPages = &maxPagesInt32
+	}
+
+	minYearParam := r.FormValue("min-year")
+	if len(minYearParam) > 0 {
+		minYear, err := strconv.ParseInt(minYearParam, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		minYearInt32 := int32(minYear)
+		filters.MinYear = &minYearInt32
+	}
+
+	maxYearParam := r.FormValue("max-year")
+	if len(maxYearParam) > 0 {
+		maxYear, err := strconv.ParseInt(maxYearParam, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		maxYearInt32 := int32(maxYear)
+		filters.MaxYear = &maxYearInt32
+	}
+
+	limitParam := r.FormValue("limit")
+	if len(limitParam) > 0 {
+		limit, err := strconv.ParseInt(limitParam, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		limitInt32 := int32(limit)
+		filters.Limit = &limitInt32
+	}
+
+	return filters, nil
+}
+
+func splitIdList(idList string) ([]int64, error) {
+	ids := make([]int64, 0)
+	idsText := strings.Split(idList, ",")
+	for _, idText := range idsText {
+		idInt, err := strconv.ParseInt(idText, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, idInt)
+	}
+	return ids, nil
 }
